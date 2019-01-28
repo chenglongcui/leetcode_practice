f21c6f659c84df8d5ccbc3731e7dc03fc2d90971
chenglongcui <chenglong.cui@yoho.cn>
2019-01-28 17:44:37 +0800

    add 704. Binary Search

---
+++ Binary_Search/704_Binary Search.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.search([-1, 0, 3, 5, 9, 12], -1)
+    print(result)
---
+++ Dynamic_Programming/120_Triangle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])
+    print(result)
