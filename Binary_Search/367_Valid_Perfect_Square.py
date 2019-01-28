93edcd334bbfa0bc82c026b90b7e599f7e497c12
chenglongcui <chenglong.cui@yoho.cn>
2019-01-28 14:13:26 +0800

    add 367. Valid Perfect Square

---
+++ Binary_Search/367_Valid_Perfect_Square.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return True
+        left, right = 0, num
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            if square < num:
+                left = mid + 1
+            elif square > num:
+                right = mid - 1
+            else:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        solution = Solution()
+        result = solution.isPerfectSquare(8)
+        print(result)
